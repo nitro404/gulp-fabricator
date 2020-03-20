3722fb7b30e14727795a2f257456cd1573e51ac4
Kevin Scroggins <nitro404@gmail.com>
2020-03-19 23:40:48 -0400

    Add unit tests for presence new functions

--- test/test.js
+++ test/test.js
@@ -6,6 +6,60 @@ const chai = require("chai");
 const expect = chai.expect;
 
 describe("Gulp Fabricator", function() {
+	describe("log", function() {
+		it("should be a function", function() {
+			expect(fabricator.log).to.be.an.instanceof(Function);
+		});
+
+		describe("info", function() {
+			it("should be a function", function() {
+				expect(fabricator.log.info).to.be.an.instanceof(Function);
+			});
+		});
+
+		describe("warn", function() {
+			it("should be a function", function() {
+				expect(fabricator.log.warn).to.be.an.instanceof(Function);
+			});
+		});
+
+		describe("error", function() {
+			it("should be a function", function() {
+				expect(fabricator.log.error).to.be.an.instanceof(Function);
+			});
+		});
+	});
+
+	describe("globSource", function() {
+		it("should be a function", function() {
+			expect(fabricator.globSource).to.be.an.instanceof(Function);
+		});
+	});
+
+	describe("noop", function() {
+		it("should be a function", function() {
+			expect(fabricator.noop).to.be.an.instanceof(Function);
+		});
+	});
+
+	describe("noopTask", function() {
+		it("should be a function", function() {
+			expect(fabricator.noopTask).to.be.an.instanceof(Function);
+		});
+	});
+
+	describe("seriesTasks", function() {
+		it("should be a function", function() {
+			expect(fabricator.seriesTasks).to.be.an.instanceof(Function);
+		});
+	});
+
+	describe("parallelTasks", function() {
+		it("should be a function", function() {
+			expect(fabricator.parallelTasks).to.be.an.instanceof(Function);
+		});
+	});
+
 	describe("setup", function() {
 		it("should be a function", function() {
 			expect(fabricator.setup).to.be.an.instanceof(Function);
@@ -15,12 +69,6 @@ describe("Gulp Fabricator", function() {
 	describe("formatOptions", function() {
 		it("should be a function", function() {
 			expect(fabricator.formatOptions).to.be.an.instanceof(Function);
-		});
-	});
-
-	describe("noop", function() {
-		it("should be a function", function() {
-			expect(fabricator.noop).to.be.an.instanceof(Function);
 		});
 	});
 
